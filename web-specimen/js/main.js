8f7c6be6a2622b32e9d37b9dbb76b9432f827710
simonthi <github@simonthiefes.de>
2022-11-23 15:53:11 +0100

    Added js to enable alteration on mouse move

---
+++ web-specimen/js/main.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+    var width = window.screen.width;
+    var height = window.screen.height;
+    document.body.addEventListener("mousemove", (event) => {
+        if (event.x >= width/2){
+            if (event.y >= height/2){
+                $("#id1").css( "font-variation-settings", "'HROT'"+30/(width/2)*(event.x-width/2)+",'VROT'"+(-((30)/(height/2)*(event.y-height/2)))+"");
+                //document.getElementById('id1').style.font-variation-settings= 'HROT' 30/height/2*(event.y-height/2);
+            } 
+
+            if (event.y < height/2) {
+                $("#id1").css( "font-variation-settings", "'HROT'"+30/(width/2)*(event.x-width/2)+",'VROT'"+(30 - (30/(height/2)*(event.y)))+"");
+            }
+        }
+        else if (event.x < width/2){
+            if (event.y >= height/2){
+                $("#id1").css( "font-variation-settings", "'HROT'"+(- (30/(width/2)*(width/2 - event.x)))+",'VROT'"+(-((30)/(height/2)*(event.y-height/2)))+"");
+            } 
+
+            if (event.y < height/2) {
+                $("#id1").css( "font-variation-settings", "'HROT'"+(- (30/(width/2)*(width/2 - event.x)))+",'VROT'"+(30 - (30/(height/2)*(event.y)))+"");
+            }
+        } 
+    });
+}
