63afa5f5a75f09fbf295cea150af1e20625f2cc1
simonthi <github@simonthiefes.de>
2022-12-20 10:56:01 +0100

    Ibid

--- template/main/fontmake.sh
+++ template/main/fontmake.sh
@@ -1,3 +1,3 @@
 fontmake 221026_octagon.glyphs -i -o ttf --output-dir ../../export/static/ttf
 fontmake 221026_octagon.glyphs -i -o otf --output-dir ../../export/static/otf
-fontmake master_ufo/octagon_var.designspace -o variable --output-dir ../../../export/static/otf
+fontmake master_ufo/octagon_var.designspace -o variable --output-dir ../../export/variable/ttf
